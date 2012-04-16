6879fda4902283e534841c0282ba2b778eb6fa64
Matt Courtney <macourtney@gmail.com>
2012-04-16 13:45:06 -0400

    Initial creation of nutritiousaurus.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject nutritiousaurus "1.0.0-SNAPSHOT"
+  :description "Nutritiousaurus is a creative commons licensed nutritional facts database."
+  :dependencies [[conjure-core "0.8.8"]
+                 [org.clojure/clojure "1.2.1"]
+                 [org.drift-db/drift-db-mysql "1.0.7"]]
+  :dev-dependencies [[lein-conjure "0.8.8"]
+                     [org.clojure/clojure "1.2.1"]])
